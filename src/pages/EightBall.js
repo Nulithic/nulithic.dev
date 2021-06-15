ac29241b36bc092eafabc6d96d25ab039986311f
nulithic <tonylieu833@gmail.com>
2021-06-15 01:38:40 -0700

    finished basic functions for 8ball

--- src/pages/EightBall.js
+++ src/pages/EightBall.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Button, Paper, Typography } from "@material-ui/core";
 
@@ -12,8 +12,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const answers = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes – definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
 const EightBall = () => {
   const classes = useStyles();
+  const [answer, setAnswer] = useState("");
+
+  const randomGen = () => {
+    return Math.floor(Math.random() * answers.length);
+  };
+
+  const handleShake = () => {
+    setAnswer(answers[randomGen()]);
+  };
 
   return (
     <div className={classes.root}>
@@ -26,9 +58,18 @@ const EightBall = () => {
           <Paper className={classes.paper}>
             <Grid item container spacing={2}>
               <Grid item xs={12}>
-                <Button variant="contained" color="secondary" style={{ whiteSpace: "nowrap" }} onClick={""} fullWidth>
-                  Spin
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  style={{ whiteSpace: "nowrap" }}
+                  onClick={handleShake}
+                  fullWidth
+                >
+                  Shake
                 </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography>{answer}</Typography>
               </Grid>
             </Grid>
           </Paper>
